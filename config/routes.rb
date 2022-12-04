890fadca05030ac53097b062416053d5cc95b708
DanielChistovaoDev <contato.danielchistovao@gmail.com>
2022-12-04 17:19:18 -0300

    config: devise token auth and create table to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api, defaults: { format: :json } do
     scope module: :v2,
       constraints: ApiConstraints.new(version: 2, default: false) do
